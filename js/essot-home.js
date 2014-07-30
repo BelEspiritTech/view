26268c4e294ef6b236a332afe209152706e9d92c
Harish Kumar Sinha <harish.sinha@belesprittech.com>
2014-07-30 20:51:46 +0530

    New Updates in the 'View' for the landing page.

--- js/essot-home.js
+++ js/essot-home.js
@@ -23,12 +23,36 @@ essotHomeApp.controller('essotHomeController', function($scope, $http) {
 		 $http.get('http://122.160.164.121:8080/essotg/rest/category/all')
 			.success(function(data) {
 				$scope.loaded = true;
-				$scope.displayCatalogues = data;						
+				$scope.displayCatalogues = data;
+				$scope.currVal = 0;
+				$scope.fadeArray = new Array();
+				
+				var categories = $scope.displayCatalogues.categories
+				for(i=0;i<categories.length; i++){
+					var categoryArray = new Array();
+					categoryArray.push("image/"+categories[i].categoryName+".jpg");
+					categoryArray.push("category.html#/"+categories[i].categoryID);
+					categoryArray.push("_self");
+					$scope.fadeArray.push(categoryArray);
+				}
+				var width =$(window).width()*0.90; 
+				var height=$(window).height()*0.79;
+				
+				var mygallery=new fadeSlideShow({   
+							wrapperid: "faddingBannerDiv", //ID of blank DIV on page to house Slideshow   
+							dimensions: [width, height], //width/height of gallery in pixels. Should reflect dimensions of largest image
+							imagearray:  $scope.fadeArray,
+							displaymode: {type:'auto', pause:1000, cycles:0, wraparound:false},   
+							persist: false, //remember last viewed slide and recall within same session?   
+							fadeduration: 2000, //transition duration (milliseconds)  
+							descreveal: "ondemand",
+							togglerid: ""
+				});
+				
 			}).error(function(err) {
 				console.log(err);
 			});
     }).error(function(err) {
       console.log(err);
    });   
-  
 });
